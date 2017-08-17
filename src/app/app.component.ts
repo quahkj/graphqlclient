dc106c87f52316b532789d05826a81e895bcd6bb
Kean Jin Quah <quahkj@yahoo.com>
2017-08-17 23:38:30 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+interface QueryResponse{
+  multiMetrics: any[];
+}
+
+const queryString = gql`
+        query ( $appId : String!, $metric : String!, $region : String!, $targets : [ String!],
+                $granularity : String!, $from : String!, $until : String!) {
+          multiMetrics(
+            applicationId : $appId,
+            metric : $metric,
+            region : $region,
+            from : $from,
+            until : $until,
+            granularity : $granularity,
+            targets : $targets,
+        ) {
+          TS
+          VL  
+        }
+      }
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  result: any[];
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo.watchQuery<QueryResponse>({
+      query: queryString,
+      variables: {
+          "appId" : "portal-local",
+          "metric" : "entity_view",
+          "region" : "vagrant",
+          "targets" : [ "0399ade3-8c9a-4941-9a31-f78a323e9260" ],
+          "granularity" : "Minutely",
+          "from" : "2017-07-18T03:00",
+          "until" : "2017-07-20T17:15:54+10:00"
+      }
+    }).subscribe(({data}) => {
+      this.result = data.multiMetrics;
+      console.log("result = " + JSON.stringify(this.result));
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { ApolloModule } from 'apollo-angular';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
+
+const client = new ApolloClient();
+
+export function provideClient(): ApolloClient {
+  return client;
+} 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApolloModule.forRoot(provideClient)
   ],
   providers: [],
   bootstrap: [AppComponent]
